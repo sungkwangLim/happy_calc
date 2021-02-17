a7700e84c31afd31519787729b7d4d7c32fbb8ff
sungkwangLim <limsungkwang@naver.com>
2021-02-17 16:58:15 +0900

    github page 서브페이지 이동 테스트

--- src/main.js
+++ src/main.js
@@ -5,5 +5,13 @@ import router from './router'
 const app = createApp(App);
 
 
-
+export default {
+    created(){
+        if (sessionStorage.redirect) {
+            const redirect = sessionStorage.redirect
+            delete sessionStorage.redirect
+            this.$router.push(redirect);
+          }
+    }
+}
 app.use(router).mount('#app');
